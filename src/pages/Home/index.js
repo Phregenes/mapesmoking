4db43eae7ce30969cd2745b3bf3f26ae096cf211
Pedro Regenes <35049850+Phregenes@users.noreply.github.com>
2020-11-21 15:55:50 -0300

    Remove: import (#2)
    * Remove: import
    
    * Edit: button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,13 @@
-
 import React from 'react';
 import './styles.css';
-import IconInstagram from '../../assents/img/instagram.svg';
 
 const Home = () => {
   return (
     <div className="Home">
       <h1 className="help">Estamos em obras, instagram</h1>
-      <a className="btninsta" href="https://www.instagram.com/mapesmoking/"><button className="bbtninsta"></button></a>
+      <a className="btninsta" href="https://www.instagram.com/mapesmoking/">
+        <button className="bbtninsta" />
+      </a>
     </div>
   );
 }
