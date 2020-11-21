1639fca493f1410ee202406ad5195801fe334aca
Pedro Regenes <phregenes@gmail.com>
2020-11-21 15:53:34 -0300

    Edit: button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,9 @@ const Home = () => {
   return (
     <div className="Home">
       <h1 className="help">Estamos em obras, instagram</h1>
-      <a className="btninsta" href="https://www.instagram.com/mapesmoking/"><button className="bbtninsta"></button></a>
+      <a className="btninsta" href="https://www.instagram.com/mapesmoking/">
+        <button className="bbtninsta" />
+      </a>
     </div>
   );
 }
