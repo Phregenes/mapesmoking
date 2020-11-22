fc319becbff628285912a677151455b2f2b7eae9
Pedro Regenes <35049850+Phregenes@users.noreply.github.com>
2020-11-22 16:07:41 -0300

    Edit: Correct images from assets (#3)

--- src/components/ComponentLanding/index.js
+++ src/components/ComponentLanding/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
-import Logo from '../../assents/img/logo.svg';
+import Logo from '../../assets/img/logo.svg';
 
 const ComponentLanding = () => {
   return (
      <div className="ComponentLanding">
-        <img className="logo" src={Logo} />
+      <img className="logo" src={Logo} />
       <div className="btn">
-      <Link to="/home">
-        <button type="button" className="btn1">
-          SIM
-        </button>
-      </Link>
-      <a className="btn" href="https://capricho.abril.com.br"><button className="btn2">NAO</button></a>
+        <Link to="/home">
+          <button type="button" className="btn1">
+            SIM
+          </button>
+        </Link>
+        <a className="btn" href="https://capricho.abril.com.br">
+          <button className="btn2">
+            NAO
+          </button>
+        </a>
       </div>
     </div>
   );
